1afd44cf73daa435ed4474744274faa2cee0e6a9
Joel Abyesiza <abyesizajoel@gmail.com>
2025-06-29 19:53:32 +0300

    more to product

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,9 +1,9 @@
-export default {
+
+
+  export default {
     providers: [
       {
-        domain: process.env.NODE_ENV === 'production' 
-          ? 'https://clerk.boosthealthinitiative.com'
-          : process.env.CLERK_ISSUER_URL || 'https://clerk.boosthealthinitiative.com',
+        domain:'https://clerk.boosthealthinitiative.com',
         applicationID: "convex",
       },
     ]
